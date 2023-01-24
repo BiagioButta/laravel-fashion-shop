c18f74a8ed1be753babd2d24941264032317390c
LuongoAndrea <andrea.luongo2002@gmail.com>
2023-01-24 10:46:17 +0100

    page 404

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        <h1>notfound</h1>
+    <div class="text-center">
+        <h1>404</h1>
+        <h3>Oops,this page not found!</h3>
+        <p>The link might be corrupted.</p>
+        <h6>or the page may have been removed</h6>
+        <button type="button" class="btn btn-dark">Go back home</button>
     </div>
 </template>
 
@@ -11,5 +15,20 @@
 </script>
 
 <style lang="scss" scoped>
+body{
+    background-color: #F4F4F4;
+}
+h1{
+    color: #030303;
+}
+h3{
+    color:#747474;
+}
+p{
+    color:#ada1a1;
+}
 
+h6{
+    color: #030303;
+}
 </style>
