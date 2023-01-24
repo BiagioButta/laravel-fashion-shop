70d2401748dfa8838ed3c0368d5c199e3e1aa118
LuongoAndrea <andrea.luongo2002@gmail.com>
2023-01-24 12:39:02 +0100

    footer e nav

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="container">
     <AppHeader></AppHeader>
-  
+    
     <main>
       <router-view></router-view>
     </main>
-
+    <AppFooter></AppFooter>
   </div>
 </template>
 
 <script>
   import AppHeader from './components/HeaderComponent.vue';
+  import AppFooter from './components/FooterComponent.vue';
   export default {
     name: 'App',
     components:{
-      AppHeader
+      AppHeader,
+      AppFooter
     }
 
   }
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -8,14 +8,10 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item" v-for="(item, index) in menuItems" :key="index">
+                    <li class="nav-item " v-for="(item, index) in menuItems" :key="index">
                         <router-link :to="{name : item.routeName}" class="nav-link">{{ item.label }}</router-link>
                 </li>
                 </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
                 </div>
             </div>
         </nav>
