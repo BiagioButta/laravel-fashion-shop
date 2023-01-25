7cec1f7f3378f0b7f0bf39143f5a381f59bcbc43
colopisalvatore <salvatorecolopi1989@gmail.com>
2023-01-25 13:36:56 +0100

    complete HeroComponent

--- src/components/HeroComponent.vue
+++ src/components/HeroComponent.vue
@@ -2,21 +2,19 @@
     <section id="hero" class="container-fluid">
         <div class="container col-12 py-5">
             <div class="row align-items-center">
-                <div class="text col-5">
-                    <h1 class="display-5 text-center fw-bold mb-3">The Best Duo-tone buildable Foundation</h1>
+                <div class="text col-4">
+                    <h1 class="display-6 text-center fw-bold mb-3">The Best Duo-tone buildable Foundation</h1>
                     <p class="lead text-center text-decoration-underline fw-bold fs-6">You're unique - your foundation should be too.
                         Create the best foundation for your skin tone with Boolgary.</p>
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                        <button type="button" class="btn btn-outline-secondary border-0 btn-lg px-4">Shop Foundation <i
-                                class="fa-solid fa-arrow-right-to-bracket"></i></button>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                        <button type="button" class="btn btn-outline-dark border-0 btn-lg px-4">Shop Foundation <i class="fa-solid fa-arrow-right-to-bracket"></i></button>
                     </div>
                 </div>                
                 <div class="col-4">
-                    <img src="../assets/image/image 1.png" class="d-block mx-lg-auto img-fluid" alt="Two Model" width="500" height="250" loading="lazy">
+                    <img src="../assets/image/image 1.png" class="d-block mx-lg-auto img-fluid" alt="Two Model" width="400" height="250" loading="lazy">
                 </div>
                 <div class="col-3">
-                    <img src="../assets/image/image 2.png" class="d-block mx-lg-auto img-fluid" alt="Model" width="250" height="250"
-                        loading="lazy">
+                    <img src="../assets/image/image 2.png" class="d-block mx-lg-auto img-fluid" alt="Model" width="250" height="250" loading="lazy">
                 </div>                
             </div>
         </div>
