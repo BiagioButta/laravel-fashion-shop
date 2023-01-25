526eac23bbe98e5289773417b764a96897809e92
colopisalvatore <salvatorecolopi1989@gmail.com>
2023-01-25 18:08:51 +0100

    restyled css

--- src/components/HeroComponent.vue
+++ src/components/HeroComponent.vue
@@ -4,7 +4,7 @@
             <div class="row align-items-center">
                 <div class="text col-4">
                     <h1 class="display-6 text-center fw-bold mb-3">The Best Duo-tone buildable Foundation</h1>
-                    <p class="lead text-center text-decoration-underline fw-bold fs-6">You're unique - your foundation should be too.
+                    <p class="lead text-center">You're unique - your foundation should be too.
                         Create the best foundation for your skin tone with Boolgary.</p>
                     <div class="d-grid gap-2 d-md-flex justify-content-md-center">
                         <button type="button" class="btn btn-outline-dark border-0 btn-lg px-4">Shop Foundation <i class="fa-solid fa-arrow-right-to-bracket"></i></button>
@@ -28,7 +28,24 @@
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/styles/main.scss";
+
 #hero{
         background-color: #fcf4ed;
+        h1{
+            font-family: 'Hanken Grotesk', sans-serif;
+            color:#3F2B1E;
+        }
+        p{
+            width: 400px;
+            color: #0D0D0D;     
+                font-family: 'Open Sans';
+                font-weight: 400;
+                font-size: 16px;
+                line-height: 27px;
+                letter-spacing: 0.005em;
+                
+                color: #0D0D0D;
+        }
     }
 </style>
