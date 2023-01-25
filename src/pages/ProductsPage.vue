d4d5279d0c98d3c3edec05f7d72314c9ec5c9448
LuongoAndrea <andrea.luongo2002@gmail.com>
2023-01-25 23:09:56 +0100

    risoluzione bug in ProductPage

--- src/pages/ProductsPage.vue
+++ src/pages/ProductsPage.vue
@@ -6,14 +6,13 @@
 
 <script>
         import axios from 'axios';
-        import CardComponent from '../components/CardComponent.vue';
         import { store } from '../store';
         export default {
         name: "ProductsPage",
         data() {
             return {
                 store,
-                projects: [],
+                product: [],
                 currentPage: 1,
                 lastPage: null,
                 total: 0,
@@ -21,12 +20,12 @@
             };
         },
         methods: {
-            getprojects(pagenum) {
+            getproduct(pagenum) {
                 axios.get(`${this.store.apiBaseUrl}/products`, { params: {
                         page: pagenum
                     } }).then((response) => {
                     //console.log(response.data.results);
-                    this.projects = response.data.results.data;
+                    this.product = response.data.results.data;
                     this.currentPage = response.data.results.current_page;
                     this.lastPage = response.data.results.last_page;
                     this.total = response.data.results.total;
@@ -40,9 +39,9 @@
             }
         },
         mounted() {
-            this.getprojects(1);
+            this.getproduct(1);
         },
-        components: { CardComponent }
+        // components: 
     }
 </script>
 
