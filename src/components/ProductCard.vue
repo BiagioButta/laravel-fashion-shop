1889f1b0aae482c17a958da07bb16a8524e3cda4
SalvatoreVillano <villano.salvatore9@gmail.com>
2023-01-26 15:42:25 +0100

    productCard implementated

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="card">
+        <!--         <img :src="`${store.imageBasePath}${post.cover_image}`" class="card-img-top">
+ -->
+        <div class="card-body text-center">
+            <h5 class="card-title py-3 text-capitalize">{{ product.name }}</h5>
+            <p class="card-text">{{ product.description }}</p>
+            <router-link class="btn btn-primary" :to="{ name: 'single-page', params: { slug: product.slug } }">
+                Vedi il post
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import { store } from '../store';
+export default {
+    name: 'ProductCard',
+    props: ['product'],
+    data() {
+        return {
+            store
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
