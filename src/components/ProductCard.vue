994263fcaf5d3672854f3cda9a0e9ea963ef8aa8
SalvatoreVillano <villano.salvatore9@gmail.com>
2023-01-28 16:17:23 +0100

    styling

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,15 +1,33 @@
 <template>
-    <div class="card">
-        <!--         <img :src="`${store.imageBasePath}${post.cover_image}`" class="card-img-top">
- -->
-        <div class="card-body text-center">
-            <h5 class="card-title py-3 text-capitalize">{{ product.name }}</h5>
-            <p class="card-text">{{ product.description }}</p>
-            <router-link class="btn btn-primary" :to="{ name: 'single-products', params: { slug: product.slug } }">
-                Vedi il post
-            </router-link>
+
+
+    <div class="col mb-5">
+        <div class="card h-100">
+            <!-- Sale badge-->
+            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
+            <!-- Product image-->
+            <img :src="`${store.imagBasePath}${product.image}`" class="card-img-top"
+                style="weight: 300px; height:450px">
+            <!-- Product details-->
+            <div class="card-body p-4">
+                <div class="text-center">
+                    <!-- Product name-->
+                    <h5 class="fw-bolder">{{ product.name }}</h5>
+                    <!-- Product price-->
+                    {{ product.price }} €
+                </div>
+            </div>
+            <!-- Product actions-->
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                <router-link class="text-center" :to="{ name: 'single-products', params: { slug: product.slug } }"><a
+                        class="btn btn-outline-dark mt-auto" href="#">Scopri</a></router-link>
+            </div>
         </div>
     </div>
+
+
+
+
 </template>
 
 <script>
